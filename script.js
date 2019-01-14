87cfe9ba69189c46bf9f303f6ab755561b63a666
Gabibi <43877968+Gabibi@users.noreply.github.com>
2019-01-14 16:32:15 +0100

    add comments

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let container = document.createElement("div")
-let container2 = document.createElement("div")
-document.body.appendChild(container)
-document.body.appendChild(container2)
-container2.hidden = true
+let container = document.createElement("div") //Creating div element
+let container2 = document.createElement("div")// Creating second div element
+document.body.appendChild(container) //Adding div in the body page
+document.body.appendChild(container2) //adding div in the body page
+container2.hidden = true // Hide the second div
 
-// 
+
 let movie = [
     ["Titre du film", "Réalisateur", "Nationalité", "Année", "Durée", "Version"],
     ["IN MY ROOM", "Ulrich Köhler", "Allemagne", "2019", "2h", "vf"],
@@ -13,14 +13,6 @@ let movie = [
     ["GRASS", "Hang Sang-Soo", "Corée du sud", "2018", "1h06", "vostfr"]
 ]
 
-let substance = [
-    ["SUBSTANCES", "Température de fusion ou solification en °C ", "Température d'ébullition en °C"],
-    ["acide acétique", "17", "118"],
-    ["acide nitrique", "-41", "86"],
-    ["acide sulfurique", "10", "290"],
-    ["alcool éthylique", "-114", "78"],
-    ["aluminium", "660", "2450"]
-]
 
 function cinema() {
     let h1 = document.createElement("h1") //Creating the h1 element
@@ -39,48 +31,64 @@ function cinema() {
 }
 cinema()
 
-
+//function that displays all the elements of the two-dimensional table
 function displayMovie(p1) {
-    let i = 0
-    let j
-    let element
-    let table = document.createElement("table")
-    let thead = document.createElement("thead")
-    let tbody = document.createElement("tbody")
-    let tr
-    let td
-    let th
-    container2.appendChild(table)
-    table.appendChild(thead)
-    table.appendChild(tbody)
-    while (i < p1.length) {
-        tr = document.createElement("tr")
-        tbody.appendChild(tr)
-        element = p1[i].length
+    let i = 0 //
+    let j //declares the variable j
+    let element //declares the variable element
+    let table = document.createElement("table") // create tag table 
+    let thead = document.createElement("thead") // create tag thead
+    let tbody = document.createElement("tbody") // create tag tbody
+    let tr //declares the variable tr
+    let td // declares the variable td
+    let th //declares the variable th
+    container2.appendChild(table)//adding table in the second div
+    table.appendChild(thead) //adding thead in table
+    table.appendChild(tbody) // adding tbody in table
+    while (i < p1.length) { //as long as i is less than the length of the array
+        tr = document.createElement("tr") //create element tr
+        tbody.appendChild(tr) //adding tr in tbody
+        element = p1[i].length //
         j = 0
-        while (j < element) {
-            if (i < 1) {
-                th = document.createElement("th")
-                thead.appendChild(tr)
-                tr.appendChild(th)
-                th.textContent = p1[i][j]
+        while (j < element) { //As long as "j" is less than "element"
+            if (i < 1) { //If "i" is less than "1"
+                th = document.createElement("th") //create tag th
+                thead.appendChild(tr) // add tr in thead
+                tr.appendChild(th) // add th in tr
+                th.textContent = p1[i][j] //adds the value of the array in "th"
             } else {
-                td = document.createElement("td")
-                tr.appendChild(td)
-                td.textContent = p1[i][j]
+                td = document.createElement("td") //create tag td
+                tr.appendChild(td) // add td in tr
+                td.textContent = p1[i][j] // adds the value of the array in "th"
             }
 
 
-            j++
+            j++ // adds 1 to the value of j
         }
-        i++
+        i++ //adds 1 to the value of i
     }
     return p1
 }
 displayMovie(movie)
 
+
+//function of the event listener that hides the first container and displays the second container
 function hidden (){
-    container.hidden = true
-    container2.hidden = false
+    container.hidden = true //hide container
+    container2.hidden = false //display container2
 }
 
+
+
+
+
+// -----------EXERCICE 2-----------
+
+let substance = [
+    ["SUBSTANCES", "Température de fusion ou solification en °C ", "Température d'ébullition en °C"],
+    ["acide acétique", "17", "118"],
+    ["acide nitrique", "-41", "86"],
+    ["acide sulfurique", "10", "290"],
+    ["alcool éthylique", "-114", "78"],
+    ["aluminium", "660", "2450"]
+]
